892a38d9a50d52dfe626d197bea3ac86b9409e67
Michał Tymiński <michal.tyminski@symmetry.pl>
2020-03-23 00:47:26 +0100

    Test add existing Vat to the set

--- src/test/java/Vat/VatRepositoryTest.java
+++ src/test/java/Vat/VatRepositoryTest.java
@@ -3,6 +3,8 @@ package Vat;
 import Product.Type;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -20,4 +22,18 @@ class VatRepositoryTest {
         //then
         assertThat(exception.getMessage()).isEqualTo("Vat for country Poland111 and product type BOOK was not found");
     }
+    
+	@Test
+	void shouldReturnTheSameSizeOfSetWhenAddExistingObj(){
+		//given
+		VatRepository repositoryToTest = new VatRepository();
+		
+		//when
+		int sizeBefore = repositoryToTest.vatValues.size();
+		repositoryToTest.addVatValue( "Poland", Type.BOOK, new BigDecimal( "0.05" ) );
+		int sizeAfter = repositoryToTest.vatValues.size();
+		//then
+		assertThat( sizeAfter - sizeBefore ).isEqualTo( 0 );
+		
+	}
 }
