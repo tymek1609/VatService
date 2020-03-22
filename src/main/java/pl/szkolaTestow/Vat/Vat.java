0a269c70ef822e6aba570d0b68d84c27cbc5c830
Michał Tymiński <michal.tyminski@symmetry.pl>
2020-03-23 00:26:54 +0100

    generated equals and hashCode method for Vat Class

---
+++ src/main/java/pl/szkolaTestow/Vat/Vat.java
@@ -0,0 +1,35 @@
+package Vat;
+
+import Product.Type;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Vat {
+    String country;
+    Type productType;
+    BigDecimal amount;
+
+    public Vat(String country, Type productType, BigDecimal amount) {
+        this.country = country;
+        this.productType = productType;
+        this.amount = amount;
+    }
+	
+	@Override
+	public boolean equals( Object o )
+	{
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		final Vat vat = (Vat) o;
+		return country.equals( vat.country ) &&
+		       productType == vat.productType &&
+		       amount.equals( vat.amount );
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash( country, productType, amount );
+	}
+}
